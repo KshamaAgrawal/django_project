6f72cfe356946e2557f86af083d2db4c8bb929b7
KshamaAgrawal <kshama@securitytroops.com>
2020-07-21 16:32:16 +0530

    django project

---
+++ blog/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render , get_object_or_404 , redirect
+from django.utils import timezone
+from .models import Post 
+from .forms import PostForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+
+# Create your views here.
+def post_list(request):
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+	return render(request, 'blog/post_list.html' , {'posts' : posts})
+	
+
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post' : post})
+
+
+def post_new(request):
+	if request.method=="POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form=PostForm()
+	return render(request, 'blog/post_edit.html', {'form' : form})	
+
+def post_edit(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	if request.method=="POST":
+		form = PostForm(request.POST, instance=post)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail' , pk=post.pk)
+	else:
+		form=PostForm(instance = post)
+	return render(request, 'blog/post_edit.html' , {'form' : form})	
+
+def signup(request):
+	if request.method == "POST":
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			post = form.save()
+			username = form.cleaned_data.get('username')
+			raw_password = form.cleaned_data.get('password1')
+			user = authenticate(username = username , password = raw_password)
+			login(request,user.username)
+			return redirect('post_list')
+	else: 
+		form = UserCreationForm()
+	return render(request, 'blog/signup.html' , {'form' : form})	
+
+def user_detail(request):
+	if request.method == "GET":
+		form = UserCreationForm(request.GET)
+		if form.is_valid():
+			username = form.request.get('username')
+			raw_password = form.request.get('password1')
+		
+			return username
+	else:
+	    form = UserCreationForm()
+	return render(request, 'blog/user_detail.html', {'form' : form})    		
---
+++ polls/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import Question , Choice
+
+# Register your models here.
+class ChoiceInline(admin.TabularInline):
+	model=Choice
+	extra = 4
+		
+
+class QuestionAdmin(admin.ModelAdmin):
+	fieldsets=[
+        (None,               {'fields': ['question_text']}),
+        ('date_information', {'fields': ['pub_date'], 'classes' : ['collapse']}),  
+	]
+	inlines = [ChoiceInline]
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
+	list_filter = ['pub_date']
+	search_field = ['question_text']
+	
+admin.site.register(Question, QuestionAdmin)
+
+
+	
